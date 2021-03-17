98ec54f6d40aa50230883ba351f853a760772ef8
Jan Lauber <jan.lauber@protonmail.ch>
2021-03-17 23:41:43 +0100

    first golang webserver tests

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Yo waddup")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/staic/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
+}
